59ee78f8b9a5d5cd10607cd85aeb078a126ecd45
Maroussia <maroussia.b@gmail.com>
2022-11-24 14:29:18 +0100

    Update code and docstring

--- searchiti.py
+++ searchiti.py
@@ -12,7 +12,7 @@ import subprocess
 ###################################################################
 
 def grepy(search:str, path:str):
-    """Returns a json list of dictionaries with the results
+    """Return a json list of dictionaries with the results
     of the search using the Python module `RipGrepy` based
     on RipGrep.
     The search is the string that will be searched in the path.
@@ -23,7 +23,7 @@ def grepy(search:str, path:str):
     return json.loads(output)
 
 def get_grepy(search:str, in_path:str, out_path:str, format='html'):
-    """Returns the output of the search with `RipGrepy`
+    """Return the output of the search with `RipGrepy`
     in a table with 3 columns:
     (1) the file_name where the search was found,
     (2) the line_number matching the search, and
@@ -33,11 +33,11 @@ def get_grepy(search:str, in_path:str, out_path:str, format='html'):
     rgpy = grepy(search, in_path)
     res = {} # empty dictionray to store the matches
     for index, match in enumerate(rgpy):
-        res[index+1] = {'paths': match['data']['path']['text'].split('/')[-1],# selects only the file_name
+        res[index+1] = {'file': match['data']['path']['text'].split('/')[-1],# selects only the file_name
                     'lines':f"{match['data']['line_number']}",
-                    'texts':f"{match['data']['lines']['text']}"}
+                    'text':f"{match['data']['lines']['text']}"}
     
-    df_res = pd.DataFrame.from_dict(res, orient='index', columns=['path', 'lines', 'text'])
+    df_res = pd.DataFrame.from_dict(res, orient='index', columns=['file', 'lines', 'text'])
     
     if format == 'html':
         return df_res.to_html(out_path)
@@ -49,7 +49,7 @@ def get_grepy(search:str, in_path:str, out_path:str, format='html'):
 ###################################################################
 
 def ripgreper(search:str, path:str, cxt=0):
-    """Returns a list of dictionaries in json format with listing all the results.
+    """Return a list of dictionaries in json format with listing all the results.
     The context (cxt) is set to 0, which means that only the line with
     the matching results will be resply in the DataFrame. To get more 
     lines displayed before and after the matching results, the cxt can
@@ -70,7 +70,7 @@ def ripgreper(search:str, path:str, cxt=0):
     return [json.loads(line) for line in result_json.stdout.decode('utf-8').splitlines()]
 
 def rg_to_df(search:str, path:str, cxt=0):
-    """Returns a Pandas DataFrame storing the match to the search.
+    """Return a Pandas DataFrame storing the match to the search.
     The context (cxt) is set to 0, which means that only the line with
     the matching results will be resply in the DataFrame. To get more 
     lines displayed before and after the matching results, the cxt can
@@ -92,29 +92,29 @@ def rg_to_df(search:str, path:str, cxt=0):
 
     res = {} # empty dictionary to store the matches
 
-    if cxt>0: # with context, the results contains the match and the cxt number of lines around the match
+    if cxt>0: # with context, the results contains the match and the number of lines (cxt) around the match
         for index, line in enumerate(json_lines):
             if line['type'] == 'match': # excludes begin, end and summary
                 # attributes to each index a dictionary that contains
-                # the path, line_number and text for each search match
+                # the file name, line_number and text for each search match
                 # and merges the context lines with the match line.
-                res[index] = {'paths': line['data']['path']['text'].split('/')[-1],
+                res[index] = {'file': line['data']['path']['text'].split('/')[-1],
                             'lines':f"{json_lines[index-1]['data']['line_number']}-{json_lines[i+1]['data']['line_number']}",
-                            'texts':f"{json_lines[index-1]['data']['lines']['text']} {line['data']['lines']['text']} {json_lines[index+1]['data']['lines']['text']}"}
+                            'text':f"{json_lines[index-1]['data']['lines']['text']} {line['data']['lines']['text']} {json_lines[index+1]['data']['lines']['text']}"}
 
     else: # without context, the result contains only the match line
         for index, line in enumerate(json_lines):
             if line['type'] == 'match': # excludes begin, end and summary
                 # creates a dict of dict that contains the path,
                 # line_number and text for each search match and
-                res[index] = {'paths': line['data']['path']['text'].split('/')[-1],
+                res[index] = {'file': line['data']['path']['text'].split('/')[-1],
                             'lines':f"{line['data']['line_number']}",
-                            'texts':f"{line['data']['lines']['text']}"}
+                            'text':f"{line['data']['lines']['text']}"}
 
-    return pd.DataFrame.from_dict(res, orient='index', columns=['paths', 'lines', 'texts'])
+    return pd.DataFrame.from_dict(res, orient='index', columns=['file', 'lines', 'text'])
 
 def rg_to_csv(out_path:str, search:str, in_path:str, cxt=0):
-    """Returns a csv file with all the matches to `search` within in_path.
+    """Return a csv file with all the matches to `search` within in_path.
     It uses RipGrep to search in the file or folder within in_path.
     The out_path is the path to the csv file in which the results will be written.
     The search is the string that will be searched in the in_path.
@@ -126,7 +126,7 @@ def rg_to_csv(out_path:str, search:str, in_path:str, cxt=0):
     return df.to_csv(out_path)
 
 def rg_to_html(out_path:str, search:str, in_path:str, cxt=0):
-    """Returns an html file with all the matches to `search` within in_path.
+    """Return an html file with all the matches to `search` within in_path.
     It uses RipGrep to search in the file or folder within in_path.
     The out_path is the path to the csv file in which the results will be written.
     The search is the string that will be searched in the in_path.
